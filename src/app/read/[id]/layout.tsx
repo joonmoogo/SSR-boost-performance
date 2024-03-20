37cf94e43d39fe39cd9157e2e487a7ceee2624bc
joonmoogo <junemuk10@gmail.com>
2024-03-20 15:17:10 +0900

    add : simple JSON Server and Fetching from Client

--- src/app/read/[id]/layout.tsx
+++ src/app/read/[id]/layout.tsx
@@ -1,6 +1,24 @@
-export default function Layout(props: { children: React.ReactNode; }){
+"use client"
+export default function Layout(props: { children: React.ReactNode; }) {
+    
+    function fetchToServer() : void{
+        fetch('http://localhost:9999/posts').then((res)=>{
+            return res.json().then((data)=>{
+                console.log(data);
+            })
+        })
+    }
 
-    return(
-        <>props.children =  {props.children}</>
+    /**  Next.js Treat Components as Server Side Component firstly! */
+
+    // If you want to use Client Side API Like UseState or UseEffect hook OR onClick Toggle
+    // Write "use client" on the top of CODE
+
+    return (
+        <>
+            <button onClick={fetchToServer}>fetch</button>
+            <br></br>
+            <>props.children =  {props.children}</>
+        </>
     )
 }
