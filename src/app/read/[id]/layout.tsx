79155fa9b3a7c674429b0243bad8ee16d9d7b19e
joonmoogo <junemuk10@gmail.com>
2024-03-20 15:39:19 +0900

    change : Client Components to Server Components

--- src/app/read/[id]/layout.tsx
+++ src/app/read/[id]/layout.tsx
@@ -1,20 +1,8 @@
-"use client"
-
-import { useEffect } from "react";
-
-export default function Layout(props: { children: React.ReactNode; }) {
+export default async function Layout(props: { children: React.ReactNode; }) {
     
-    function fetchToServer() : void{
-        fetch('http://localhost:9999/posts').then((res)=>{
-            return res.json().then((data)=>{
-                console.log(data);
-            })
-        })
-    }
-
-    useEffect(()=>{
-        fetchToServer();
-    },[])
+    const serverData = await fetch('http://localhost:9999/posts');
+    const posts = await serverData.json();
+    console.log(posts);
 
     /**  Next.js Treat Components as Server Side Component firstly! */
 
