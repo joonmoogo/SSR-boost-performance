d166da40c1abaed359e459272d5ef0de6ad30da6
joonmoogo <junemuk10@gmail.com>
2024-04-09 16:05:28 +0900

    이너조인

--- sql/personal.ts
+++ sql/personal.ts
@@ -1,6 +1,6 @@
 export const personalSQL ={
     getAllPersonals(){
-        return `SELECT * FROM personal`;
+        return `SELECT * FROM personal INNER JOIN personal_images ON personal.id = personal_images.personal_id `;
     },
 
     postOnePersonal(){
