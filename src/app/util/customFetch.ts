4514ee1e3265616560884995cae60f94cd1a89c7
joonmoogo <junemuk10@gmail.com>
2024-04-23 20:46:48 +0900

    server-data chching

--- src/app/util/customFetch.ts
+++ src/app/util/customFetch.ts
@@ -2,19 +2,19 @@ import config from "../config/config";
 import { techDTO, personalDTO, reelsDTO } from "@/types/DTO";
 
 const getAllTech = async () => {
-    const serverData = await fetch(`${config.localUrl}/api/tech`, { cache: 'no-store' });
+    const serverData = await fetch(`${config.localUrl}/api/tech`, { cache: 'no-store', next: { revalidate: 60 } },);
     const data: techDTO[] = await serverData.json();
     return data;
 }
 
 const getAllPersonal = async () => {
-    const serverData = await fetch(`${config.localUrl}/api/personal`, { cache: 'no-store' });
+    const serverData = await fetch(`${config.localUrl}/api/personal`, { cache: 'no-store', next: { revalidate: 60 } });
     const data: personalDTO[] = await serverData.json();
     return data;
 }
 
 const getAllReels = async () => {
-    const serverData = await fetch(`${config.localUrl}/api/reels`, { cache: 'no-store' });
+    const serverData = await fetch(`${config.localUrl}/api/reels`, { cache: 'no-store', next: { revalidate: 60 } });
     const data: reelsDTO[] = await serverData.json();
     return data;
 }
@@ -29,9 +29,9 @@ const deleteOnerPersonal = async () => {
     console.log(data);
 }
 
-const getOneTech =  async(id:any) =>{
-    const serverData = await fetch(`${config.localUrl}/api/tech/${id}`,{cache: 'no-store'});
-    const data : any = await serverData.json();
+const getOneTech = async (id: any) => {
+    const serverData = await fetch(`${config.localUrl}/api/tech/${id}`, { cache: 'no-store', next: { revalidate: 60 } });
+    const data: any = await serverData.json();
     return data;
 }
 
