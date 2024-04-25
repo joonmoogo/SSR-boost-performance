6eaab80898de646b9d63468e963d91f112d9b87d
joonmoogo <junemuk10@gmail.com>
2024-04-26 03:28:50 +0900

    issue #20 masonry UI 적용

--- src/app/components/PersonalBox.tsx
+++ src/app/components/PersonalBox.tsx
@@ -7,13 +7,14 @@ import { deleteOnerPersonal } from "../util/customFetch";
 import { timeTune } from "../util/util";
 
 
-export default function PersonalBox(props: { item: personalDTO }) {
+export default function PersonalBox(props: { item: personalDTO, viewport: any }) {
 
     const slideshow = useRef<any>(null);
     const [currentSlide, setCurrentSlide] = useState<number>(0);
     const [currentValue, setCurrentValue] = useState<number>(0);
     const imageArray = props.item.image_url.split(',');
     const slideCount = imageArray.length
+    const isMobile = props.viewport === 'mobile' ? true : false
 
     // IMAGE SLIDESHOW : n * -45
     // ex) slideCount == 2 ? max = -90 
@@ -47,32 +48,55 @@ export default function PersonalBox(props: { item: personalDTO }) {
         // slideshow.current.style.transform = `translate(${0}vw)`;
     }
     return (
-        <div className="image-box">
-            <div className="box-header">
-                <div className="author">
-                    <img src="ssepcat.png"></img>
-                    <p>{config.username}</p>
-                    <p>{timeTune(props.item.created_at)}</p>
+        <>
+            {isMobile ?
+                /* Mobile UI */
+                <div className="image-box">
+                    <div className="box-header">
+                        <div className="author">
+                            <img src="ssepcat.png"></img>
+                            <p>{config.username}</p>
+                            <p>{timeTune(props.item.created_at)}</p>
+                        </div>
+                    </div>
+                    <div className="box-content">
+                        {props.item.content}
+                    </div>
+                    <div className="box-image"
+                        ref={slideshow}
+                        onTouchStart={onTouchStart}
+                        onTouchEnd={onTouchEnd}
+                        onTouchMove={onTouchMove}
+                        style={{
+                            transition: 'transform 0.5s'
+                        }}
+                    >
+                        {isMobile ? imageArray.map((e, index) => {
+                            return (
+                                <img key={index} src={`static/personal_images/${e}`} alt="" />
+                            )
+                        }) : <img src={`static/personal_images/${imageArray[0]}`}></img>}
+                    </div>
                 </div>
-            </div>
-            <div className="box-content">
-                {props.item.content}
-            </div>
-            <div className="box-image"
-                ref={slideshow}
-                onTouchStart={onTouchStart}
-                onTouchEnd={onTouchEnd}
-                onTouchMove={onTouchMove}
-                style={{
-                    transition: 'transform 0.5s'
-                }}
-            >
-                {imageArray.map((e, index) => {
-                    return (
-                        <img key={index} src={`static/personal_images/${e}`} alt="" />
-                    )
-                })}
-            </div>
-        </div>
+                :
+                /* Desktop UI */
+                <div className="image-box">
+                    <div className="box-image">
+                        <img src={`static/personal_images/${imageArray[0]}`} />
+                    </div>
+                    <div className="box-header">
+                        <div className="author">
+                            <img src="ssepcat.png"></img>
+                            <p>{config.username}</p>
+                            <p>{timeTune(props.item.created_at)}</p>
+                        </div>
+                    </div>
+                    <div className="box-content" ref={slideshow}>
+                        {props.item.content}
+                    </div>
+                </div>
+            }
+
+        </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,16 @@ import ImageBox from './components/PersonalBox';
 import config from './config/config';
 import { techDTO,personalDTO,reelsDTO } from "@/types/DTO";
 import {getAllPersonal} from "@/app/util/customFetch"
-export default async function Home() {
+export default async function Home(request:any) {
   const data: personalDTO[] = await getAllPersonal();
   console.log(data);
+  const {viewport} = request.searchParams
   return (
     <>
       <div className="main-page">
         {data.map((post: personalDTO, index: number) => {
           return (
-            <ImageBox key={index} item={post} />
+            <ImageBox key={index} item={post} viewport={viewport} />
           )
         })}
       </div>
