a40687094eaa2773b571fbbf6b90acd0ac5b5aa6
joonmoogo <junemuk10@gmail.com>
2024-05-03 09:10:50 +0900

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server'
- 
+
 export function middleware(request: NextRequest) {
   const url = request.nextUrl
   const { device } = userAgent(request)
   const viewport = device.type === 'mobile' ? 'mobile' : 'desktop'
-  url.searchParams.set('viewport', viewport)
-  return NextResponse.rewrite(url)
+  // url.searchParams.set('viewport', viewport);
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('x-pathname', request.nextUrl.pathname);
+  requestHeaders.set('x-viewport',viewport);
+  
+  const response= NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    }
+  });
+
+  return response
 }
