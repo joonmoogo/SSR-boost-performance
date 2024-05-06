abfb282353c2df2bca62eb3d3ceea03538e4911e
joonmoogo <junemuk10@gmail.com>
2024-05-07 02:59:30 +0900

    issue #20 homepage - section1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import './globals.scss'
 import Link from 'next/link';
 import ImageBox from './_components/PersonalBox';
@@ -9,33 +10,42 @@ export default async function Home(request: any) {
   // console.log(data);
   const { viewport } = request.searchParams
   return (
-    <>
-      <div className="introduce-page">
-        {/* {data.map((post: personalDTO, index: number) => {
-          return (
-            <ImageBox key={index} item={post} viewport={viewport} />
-          )
-        })} */}
+    <div>
+      <section id='one'>
+        <div className="introduce-page">
+          <div className="left-side">
+            <div className="left-header">
+              Web Developer
+            </div>
+            <div className="left-content">
+              Agenecy-level<br /> web development
+              services at <br />freelancer rates
+            </div>
+            <div className="left-description">
+              My mission is to design and develop<br />
+              a website that you and your audience love
+            </div>
+          </div>
+          <div className="right-side">
+            <div className="right-image">
+            </div>
+          </div>
 
-        <div className="left-side">
-          <div className="left-header">
-            Frontend Developer
-          </div>
-          <div className="left-content">
-            Agenecy-level web  development
-            services at freelancer rates
-          </div>
-          <div className="left-description">
-          My mission is to design and develop a website that you and your audience love  
-          </div>
-        </div>
-        <div className="right-side">
-          <div className="right-image">
-          </div>
-        </div>
+          {/* {data.map((post: personalDTO, index: number) => {
+        return (
+          <ImageBox key={index} item={post} viewport={viewport} />
+        )
+      })} */}
 
-      </div>
-    </>
+        </div >
+      </section >
+      <section id='two'>
+        <div className="introduce-page">section2</div>
+      </section>
+      <section id='three'>
+        <div className="introduce-page">section3</div>
+      </section>
+    </div>
 
   );
 }
