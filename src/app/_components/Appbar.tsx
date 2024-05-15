f691dd0b83d26d06dea45f10e32208d3a7dfe0b6
joonmoogo <junemuk10@gmail.com>
2024-05-16 04:31:27 +0900

    fix error

--- src/app/_components/Appbar.tsx
+++ src/app/_components/Appbar.tsx
@@ -1,16 +1,12 @@
+
 import React from "react";
 import config from "../_config/config";
 import "../_styles/appbar.scss"
 import Link from "next/link";
-import { NextRequest } from "next/server";
-import SideNav from "./SideNav";
-import { headers } from "next/headers";
 
 export default function Appbar() {
 
-    const headerList = headers();
-    const viewport = headerList.get('x-viewport') as string;
-
+    
     return (
         <div className="appbar">
             <Link className="appbar-content" role="appbar-content" href={'/'}>
