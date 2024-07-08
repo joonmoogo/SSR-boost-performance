01cb2cb56b87af3bc79fa73f84278bd0aaf29b62
joonmoogo <junemuk10@gmail.com>
2024-07-09 01:39:42 +0900

    일단 postgres 연결 성공?

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -2,10 +2,26 @@ import { sql } from '@vercel/postgres'
 import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const result = await sql`CREATE TABLE pets (Name varchar(255) Owner varchar(255))`
+
+        // CREATE TABLE IF NOT EXISTS tech (
+        //     id SERIAL PRIMARY KEY,
+        //     title VARCHAR(255),
+        //     first_div VARCHAR(255),
+        //     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        //     content TEXT
+        // );
+        const result = await sql`CREATE TABLE IF NOT EXISTS tech (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255),
+            first_div VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            content TEXT
+        );`
+        console.log(result)
         return res.status(200).json({ result })
     }
     catch (error) {
+        console.log(error)
         return res.status(500).json({ error })
     }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,41 +12,22 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: `${config.name} : 준무고 잡다구리 보관함`,
 }
-// export default function Home(request: any) {
-
-//   return (
-//     <>
-//       <div className="main-page">
-//         asd
-//       </div>
-
-
-
-//       <div className="mobile-container">
-//         Enthusiastic <br /> web developer<br /><br />
-//         Thrive on <br />problem-solving <br />and teamwork.<br /><br />
-//         My mission is to design and develop
-//         a website that you and your audience love
-//       </div >
-//     </>
-//   );
-// }
-import { sql } from "@vercel/postgres";
-
-export default async function Cart({
-  params
-} : {
-  params: { user: string }
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+export default function Home(request: any) {
 
   return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="main-page">
+        asd
+      </div>
+
+
+
+      <div className="mobile-container">
+        Enthusiastic <br /> web developer<br /><br />
+        Thrive on <br />problem-solving <br />and teamwork.<br /><br />
+        My mission is to design and develop
+        a website that you and your audience love
+      </div >
+    </>
   );
 }
